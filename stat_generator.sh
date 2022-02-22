1462ba57388516d344edaef483feb3706ade654d
Veej17 <veerlevwinden@hotmail.com>
2022-02-22 16:36:36 +0000

    Add files via upload

--- stat_generator.sh
+++ stat_generator.sh
@@ -3,32 +3,52 @@
 # Defaults
 USER='neo4j'
 
-while getopts p:r: flag
-do
+help() {
+  # Display Help
+  echo "Description of the required inputs of this script."
+  echo
+  echo "The following inputs are required -p and -r:"
+  echo "r     The desired repository - 'dev' or 'prod'"
+  echo "p     The Neo4J password connected to the repository"
+  echo
+}
+
+while getopts p:r:h flag; do
   case "${flag}" in
-    p) PW="${OPTARG}" ;;
-    r) REPOSITORY="${OPTARG}" ;;
-    \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
-    :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
-    *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+  p) PW="${OPTARG}" ;;
+  r) REPOSITORY="${OPTARG}" ;;
+  h) help ;;
+  \?)
+    echo "Unknown option: -$OPTARG, add -h for help" >&2
+    exit 1
+    ;;
+  :)
+    echo "Missing option argument for -$OPTARG, add -h for help" >&2
+    exit 1
+    ;;
+  *)
+    echo "Unimplemented option: -$OPTARG, add -h for help" >&2
+    exit 1
+    ;;
   esac
 done
 
 # Control mandatory arguments
 if [ ! "$PW" ] || [ ! "$REPOSITORY" ]; then
-  echo "arguments -p and -r must be provided"
-  echo >&2; exit 1
+  echo "arguments -p and -r must be provided, add -h for help."
+  echo >&2
+  exit 1
 fi
 
 # Check what repository to use, dev or prod. And set the correct database and branch for it
 if [[ "$REPOSITORY" == "dev" ]]; then
   DATABASE="bolt://intact-neo4j-001.ebi.ac.uk:7687"
   GIT_REP="statistics_dev"
-fi
-
-if [[ "$REPOSITORY" == "prod" ]]; then
+elif [[ "$REPOSITORY" == "prod" ]]; then
   DATABASE="bolt://intact-neo4j-003.ebi.ac.uk:7687"
   GIT_REP="statistics_prod"
+else
+  echo "Incorrect argument for -r, add -h for help."
 fi
 
 # set brench and install requirements
@@ -36,6 +56,7 @@ git checkout ${GIT_REP}
 pip3 install -r requirements.txt
 if [ $? -ne 0 ]; then
   echo "Requirements installation failed, please take a look at the requirements."
+  exit 1
 fi
 
 # Run python script
