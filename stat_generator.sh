e6ae50e25a9d2d844a192e4b6c66205114f394ed
EliotRagueneau <eragueneau@ebi.ac.uk>
2022-07-18 11:36:19 +0100

    Harlow migration of graph db

--- stat_generator.sh
+++ stat_generator.sh
@@ -42,10 +42,10 @@ fi
 
 # Check what repository to use, dev or prod. And set the correct database and branch for it
 if [[ "$REPOSITORY" == "dev" ]]; then
-  DATABASE="bolt://intact-neo4j-001.ebi.ac.uk:7687"
+  DATABASE="bolt://intact-neo4j-001-hl.ebi.ac.uk:7687"
   GIT_REP="statistics_dev"
 elif [[ "$REPOSITORY" == "prod" ]]; then
-  DATABASE="bolt://intact-neo4j-003.ebi.ac.uk:7687"
+  DATABASE="bolt://intact-neo4j-003-hl.ebi.ac.uk:7687"
   GIT_REP="statistics_prod"
 else
   echo "Incorrect argument for -r, add -h for help."
