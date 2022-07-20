c345bbb28b82d94e0f1c1babe3a2bd7d0cca6963
EliotRagueneau <eragueneau@ebi.ac.uk>
2022-07-20 16:49:13 +0100

    New statistics files added on Wed Jul 20 16:49:12 BST 2022

--- stat_generator.sh
+++ stat_generator.sh
@@ -66,6 +66,7 @@ fi
 
 if python3 statistics_generator.py --database ${DATABASE} --user ${USER} --pw "${PW}" --release "${RELEASE_VERSION}"; then
   echo "Script executed successfully"
+  git add output_data/*
   git checkout ${GIT_REP}
   git merge main
   git commit -a -m "New statistics files added on $(date)"
